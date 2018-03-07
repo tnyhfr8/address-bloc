7030fd46cf80396826ad1710ec5efdbf433b50eb
Tony Hoefer <tnyhfr@gmail.com>
2018-03-06 19:55:22 -0800

    Assignment 6 Edits

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -121,6 +121,29 @@ RSpec.describe AddressBook do
       # Check the size of the entries in AddressBook
       expect(book_size).to eq 3
     end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries.csv")
+      # Check the first entry
+      entry_one = book.entries[0]
+
+      # #5
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+
+    it "imports the 2nd entry" do
+       book.import_from_csv("entries.csv")
+       # Check the second entry
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+     end
+
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries.csv")
+       # Check the third entry
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+     end
   end
 
   # Test the binary_search method
